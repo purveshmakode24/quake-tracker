ae64674c03f9df219d4759e2bd9fcd32f9959194
Purvesh Makode <makodepurvesh@gmail.com>
2020-06-09 20:54:10 +0530

    remove unnecessary zoom from app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zoom: 3,
       defaultEarthquakesData: [],  // [{}, {}, {},----]
       serverNotConnectedMsg: '',
       isLoaded: false
