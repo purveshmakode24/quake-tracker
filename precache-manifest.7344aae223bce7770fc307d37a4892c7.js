040b71e3d846fbda2092182e7a1f040a804a3f35
Purvesh Makode <makodepurvesh@gmail.com>
2020-06-14 11:00:20 +0530

    Updates

---
+++ precache-manifest.7344aae223bce7770fc307d37a4892c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbcb167acef47786f492290dfc0e169",
+    "url": "/quake-tracker/index.html"
+  },
+  {
+    "revision": "9cb78a0beb34c720b96a",
+    "url": "/quake-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0df3cf58f96cae5e5e93",
+    "url": "/quake-tracker/static/css/main.6b784630.chunk.css"
+  },
+  {
+    "revision": "9cb78a0beb34c720b96a",
+    "url": "/quake-tracker/static/js/2.814887fc.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/quake-tracker/static/js/2.814887fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df3cf58f96cae5e5e93",
+    "url": "/quake-tracker/static/js/main.dd6021df.chunk.js"
+  },
+  {
+    "revision": "c59aabad96e44b41e9bd",
+    "url": "/quake-tracker/static/js/runtime-main.c96aa38b.js"
+  },
+  {
+    "revision": "6ce3ca72c0c5de74f48dd1151c2a5e27",
+    "url": "/quake-tracker/static/media/red-marker.6ce3ca72.png"
+  }
+]);
