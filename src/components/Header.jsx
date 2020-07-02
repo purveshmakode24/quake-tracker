0ea8ffd1da77a9238a2902b3eb1e85e622da1a8c
Purvesh Makode <makodepurvesh@gmail.com>
2020-07-02 10:33:01 +0530

    Fix iframe attributes due to JSX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ class Header extends Component {
                     {/* <span className="nav-followlnk-span"><a href="https://purveshmakode.com">{`</>`} w/ &#10084; by PM</a></span> <br /> */}
                 </p>
                 <p className="iframe">
-                <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.quaketracker.gq&width=122&layout=button_count&action=like&size=small&share=true&height=46&appId" title="fb_like_share_btn" width="122" height="22" style={{ border: "none", overflow: "hidden" }} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.quaketracker.gq&width=122&layout=button_count&action=like&size=small&share=true&height=46&appId" title="fb_like_share_btn" width="122" height="22" style={{ border: "none", overflow: "hidden" }} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                 </p>
 
             </header>
